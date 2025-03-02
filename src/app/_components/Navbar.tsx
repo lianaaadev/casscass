616151cde04ee88157bb3a1f4ef982082dc86c0e
Liana Kan <liana.kan98@gmail.com>
2025-03-01 23:21:19 -0800

    remove collapsible menu to simplify user experience

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,47 +1,18 @@
-"use client";
-
-import { useState, JSX } from "react";
+import { JSX } from "react";
 import Link from "next/link";
-import { Menu, X } from "lucide-react";
 
 export default function Navbar(): JSX.Element {
-  const [menuOpen, setMenuOpen] = useState<boolean>(false);
-
   return (
     <nav className="autour-one-regular">
       <div>
         <div className="flex justify-center items-center h-16">
-          <div className="hidden md:flex space-x-6">
+          <div className="flex space-x-6">
             <NavLink href="/">Art</NavLink>
             <NavLink href="/about">About</NavLink>
             <NavLink href="/contact">Contact</NavLink>
           </div>
-
-          <div className="md:hidden">
-            <button
-              onClick={() => setMenuOpen(!menuOpen)}
-              aria-label="Toggle Menu"
-              className="text-orange-700"
-            >
-              {menuOpen ? <X size={24} /> : <Menu size={24} />}
-            </button>
-          </div>
         </div>
       </div>
-
-      {menuOpen && (
-        <div className="md:hidden p-4">
-          <NavLink href="/" onClick={() => setMenuOpen(false)}>
-            Art
-          </NavLink>
-          <NavLink href="/about" onClick={() => setMenuOpen(false)}>
-            About
-          </NavLink>
-          <NavLink href="/contact" onClick={() => setMenuOpen(false)}>
-            Contact
-          </NavLink>
-        </div>
-      )}
     </nav>
   );
 }

3eef88a6dbb1d7e4ed23a714fa3cdf0c7de49d92
Liana Kan <liana.kan98@gmail.com>
2025-03-01 23:24:03 -0800

    add netlify form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,16 +4,28 @@ import ClickableLogo from "@components/ClickableLogo";
 export default function About() {
   return (
     <div className="max-w-7xl mx-auto p-5">
-     <div className="mt-9 flex justify-center">
+      <div className="mt-9 flex justify-center">
         <ClickableLogo />
       </div>
       <div className="mb-9">
         <Navbar />
       </div>
 
-      <div>
-        <p>email email dot com...</p>
-      </div>
+      <form name="contact" netlify>
+        <p>
+          <label>
+            Name <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   );
 }
